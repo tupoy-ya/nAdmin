90a93a45298b527e3962ce70d98a47769a98397b
nazarbaev <59479158+sarry13@users.noreply.github.com>
2021-02-11 14:37:07 +0600

    Add files via upload

--- lua/nadmin/modules/playtime.lua
+++ lua/nadmin/modules/playtime.lua
@@ -14,25 +14,91 @@ if CLIENT or SERVER then
 	end
 
 	function meta:SetTotalTime(n)
-		ply:SetNWInt("TotalTime", tonumber(n))
+		ply:SetNWInt("TotalTime", tonumber(n) or ply:GetTotalTime() or 0)
 	end
 end
 
 if SERVER then
+    if game.SinglePlayer() then
+		nAdmin.Print("Вы находитесь в одиночной игре. Модуль playtime не будет включён.")
+		return
+	end
+
+    require'mysqloo'
+
+	if not mysqloo then
+		nAdmin.Print("gmsv_mysqloo не найден. Playtime не будет включён.")
+		return
+	end
+
+	if not file.Exists("nadmin/dbcfg.txt", "DATA") then
+		file.Write("nadmin/dbcfg.txt", util.TableToJSON({["url"] = "", ["login"] = "", ["pass"] = "", ["dbName"] = "", ["port"] = 3306}))
+	end
+	local mysqlConnect = util.JSONToTable(file.Read("nAdmin/dbcfg.txt", "DATA"))
+	nAdminDB = mysqloo.connect(mysqlConnect["url"], mysqlConnect["login"], mysqlConnect["pass"], mysqlConnect["dbName"], math.Round(mysqlConnect["port"]))
+
+	local meta = FindMetaTable'Player'
+
+	function meta:SetPTime(TIME)
+		local ACID = self:AccountID()
+		local Q = nAdminDB:query("REPLACE INTO nAdmin_time (infoid, time) VALUES (" .. SQLStr(ACID) .. ", " .. SQLStr(TIME) .. ")")
+		function Q:onError(err)
+			nAdmin.Print("Запрос выдал ошибку: " .. err)
+		end
+		Q:start()
+	end
+
+	function meta:GetPTime(func)
+		local ACID = self:AccountID()
+		local Q = nAdminDB:query("SELECT time FROM nAdmin_time WHERE infoid = " .. SQLStr(ACID) .. " LIMIT 1")
+		function Q:onError(err)
+			nAdmin.Print("Запрос выдал ошибку: " .. err)
+		end
+		Q:start()
+		function Q:onSuccess(data)
+			if data and data[1] then
+				func(data[1].time)
+			else
+				func(0)
+			end
+		end
+	end
+
+	function meta:RemovePTime()
+		local ACID = self:AccountID()
+		local Q = nAdminDB:query("DELETE FROM nAdmin_time WHERE infoid = " .. SQLStr(ACID))
+		function Q:onError(err)
+			nAdmin.Print("Запрос выдал ошибку: " .. err)
+		end
+		Q:start()
+	end
+
+	function nAdminDB:onConnected()
+		nAdmin.Print("База данных успешно подключена.")
+	end
+
+	function nAdminDB:onConnectionFailed( err )
+		print("Ошибка подключения к базе данных!")
+		print("Ошибка:", err)
+	end
+	nAdminDB:connect()
+
     hook.Add("PlayerInitialSpawn", "PTime", function(ply)
         ply:SetNWInt("StartTimeSession", CurTime())
-        ply:SetNWInt("TotalTime", tonumber(ply:GetPData("TimeOnServer", 0)))
+		ply:GetPTime(function(a)
+			ply:SetNWInt("TotalTime", a)
+		end)
     end)
 
     hook.Add("PlayerDisconnected", "PTime", function(ply)
-        ply:SetPData("TimeOnServer", ply:GetTotalTime())
+        ply:SetPTime(ply:GetTotalTime())
     end)
 
 	local function savePTime()
 		for _, ply in ipairs(player.GetAll()) do
-			ply:SetPData("TimeOnServer", ply:GetTotalTime())
+			ply:SetPTime(ply:GetTotalTime())
 		end
 	end
 
-	timer.Create("savePTime", 180, 0, savePTime)
+	timer.Create("savePTime", 120, 0, savePTime)
 end
--- lua/nadmin/server/sv_usergroup.lua
+++ lua/nadmin/server/sv_usergroup.lua
@@ -9,11 +9,16 @@ end
 
 function meta:SetUserGroup(group)
 	self:SetNWString("usergroup", group)
-	for k, v in next, SteamIDs do
-		if k == self:SteamID() then
-			SteamIDs[k].group = group
-			file.Write("nadmin/users.txt", util.TableToJSON(SteamIDs))
-			break
+	timer.Simple(0, function()
+		self:SetTeam(_Global_Teams[group].num)
+	end)
+	if not SteamIDs[self:SteamID()] then
+		for k, v in next, SteamIDs do
+			if k == self:SteamID() then
+				SteamIDs[k].group = group
+				file.Write("nadmin/users.txt", util.TableToJSON(SteamIDs))
+				break
+			end
 		end
 	end
 end
@@ -36,18 +41,15 @@ end
 
 LoadUsers()
 
-hook.Add("PlayerAuthed", "PlayerAuthSpawn", function(ply)
+hook.Add("PlayerInitialSpawn", "PlayerAuthSpawn", function(ply)
 	local steamid = ply:SteamID()
 	if (game.SinglePlayer() or ply:IsListenServerHost()) then
 		ply:SetUserGroup("superadmin")
-		ply:SetTeam(_Global_Teams["superadmin"].num)
 		return
 	end
 	if (SteamIDs[steamid] == nil) then
 		ply:SetUserGroup("user")
-		ply:SetTeam(_Global_Teams["user"].num)
 		return
 	end
 	ply:SetUserGroup(SteamIDs[steamid].group)
-	ply:SetTeam(_Global_Teams[SteamIDs[steamid].group].num)
 end)
