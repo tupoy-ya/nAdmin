210bd19b589054bf91528f9b59e31c1f18b8606c
nazarbaev <59479158+sarry13@users.noreply.github.com>
2021-02-28 15:58:54 +0600

    Delete nadmin/lua directory

--- nadmin/lua/autorun/n_load.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile("nadmin/sh_func.lua")
-include("nadmin/sh_func.lua")
-
-if CLIENT then
-	for k, v in ipairs(file.Find("nadmin/client/*", "LUA")) do
-		include("nadmin/client/" .. v)
-	end
-end
