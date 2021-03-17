e4c1d8a9a888d069069476201f23b5be2a1b2954
nazarbaev <59479158+sarry13@users.noreply.github.com>
2021-03-17 19:26:50 +0600

    нахуй не нада

--- lua/nadmin/server/sv_misc.lua
+++
@@ -1,8 +0,0 @@
-hook.Add("PlayerInitialSpawn", "FullLoadSetup", function(ply)
-	hook.Add("SetupMove", ply, function(self, ply, _, cmd)
-		if self == ply and not cmd:IsForced() then
-			hook.Run("PlayerFullLoad", self)
-			hook.Remove("SetupMove", self)
-		end
-	end)
-end)
